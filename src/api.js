a3897ebb602ccae463964cc4e5ed507727923c68
paulinagzd <paulinagdavalos@gmail.com>
2020-10-26 23:35:11 -0600

    final con base de datos

--- src/api.js
+++ src/api.js
@@ -1,39 +1,37 @@
-import Vue from 'vue'
-import axios from 'axios'
+import Vue from 'vue';
+import axios from 'axios';
 
 const client = axios.create({
   baseURL: 'http://localhost:8081/',
-  json: true
-})
+  json: true,
+});
 
 export default {
-  async execute (method, resource, data) {
+  async execute(method, resource, data) {
     // inject the accessToken for each request
-    let accessToken = await Vue.prototype.$auth.getAccessToken()
+    const accessToken = await Vue.prototype.$auth.getAccessToken();
     return client({
       method,
       url: resource,
       data,
       headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    }).then(req => {
-      return req.data
-    })
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }).then(req => req.data);
   },
-  getPosts () {
-    return this.execute('get', '/posts')
+  getPosts() {
+    return this.execute('get', '/posts');
   },
-  getPost (id) {
-    return this.execute('get', `/posts/${id}`)
+  getPost(id) {
+    return this.execute('get', `/posts/${id}`);
   },
-  createPost (data) {
-    return this.execute('post', '/posts', data)
+  createPost(data) {
+    return this.execute('post', '/posts', data);
   },
-  updatePost (id, data) {
-    return this.execute('put', `/posts/${id}`, data)
+  updatePost(id, data) {
+    return this.execute('put', `/posts/${id}`, data);
   },
-  deletePost (id) {
-    return this.execute('delete', `/posts/${id}`)
+  deletePost(id) {
+    return this.execute('delete', `/posts/${id}`);
   },
-}
+};
